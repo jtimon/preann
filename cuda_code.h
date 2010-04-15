5a37c1872ec454860a919326b7f64110ae728052
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2010-04-15 18:20:03 +0000

    RESTORING
    git-svn-id: svn://svn.code.sf.net/p/preann/code@41 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

--- cuda_code.h
+++ cuda_code.h
@@ -5,8 +5,14 @@
 
 #define THREADS_PER_BLOCK 64
 
-void checkCUDAError(const char *msg);
+extern "C" void* cuda_malloc(unsigned byteSize);
+extern "C" void cuda_free(void* d_ptr);
+extern "C" void cuda_copyToDevice(void* d_dest, void* h_src, unsigned count);
+extern "C" void cuda_copyToHost(void* h_dest, void* d_src, unsigned count);
+extern "C" void cuda_setZero(void* data, unsigned byteSize, VectorType vectorType, unsigned block_size);
+extern "C" float* cuda_getNegativeThresholds(float* thresholds, unsigned size, unsigned block_size);
 
+extern "C" void cuda_activation(void* data, unsigned size, VectorType vectorType, float* results, FunctionType functionType, unsigned block_size);
 
 extern "C" void cuda_inputCalculation(void* inputPtr, unsigned input_size, VectorType inputType, unsigned output_size, void* weighs, float* results, unsigned block_size);
 extern "C" void cuda_inputCalculation2(void* inputPtr, unsigned input_size, VectorType inputType, unsigned output_size, void* weighs, float* results, unsigned block_size);
