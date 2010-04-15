775e6af1a7cae0bde0011a5bdbd8c46647b4c13d
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2010-04-15 18:11:32 +0000

    git-svn-id: svn://svn.code.sf.net/p/preann/code@39 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

---
+++ cudaVector.cpp
@@ -0,0 +1,65 @@
+/*
+ * cudaVector.cpp
+ *
+ *  Created on: Mar 25, 2010
+ *      Author: timon
+ */
+
+#include "cudaVector.h"
+
+CudaVector::CudaVector(unsigned size, VectorType vectorType)
+{
+	this->size = size;
+	this->vectorType = vectorType;
+	unsigned byte_sz = getByteSize();
+	data = cuda_malloc(byte_sz);
+
+	cuda_setZero(data, byte_sz, vectorType, THREADS_PER_BLOCK);
+}
+
+CudaVector::~CudaVector()
+{
+	if (data) {
+		cuda_free(data);
+		data = NULL;
+	}
+}
+
+void CudaVector::free()
+{
+	if (data) {
+		cuda_free(data);
+	}
+}
+
+void CudaVector::copyFrom(Interface *interface)
+{
+	if (size < interface->getSize()){
+		string error = "The Interface is greater than the Vector.";
+		throw error;
+	}
+	if (vectorType != interface->getVectorType()){
+		string error = "The Type of the Interface is different than the Vector Type.";
+		throw error;
+	}
+	cuda_copyToDevice(data, interface->getDataPointer(), interface->getByteSize());
+}
+
+void CudaVector::copyTo(Interface *interface)
+{
+	if (interface->getSize() < size){
+		string error = "The Vector is greater than the Interface.";
+		throw error;
+	}
+	if (vectorType != interface->getVectorType()){
+		string error = "The Type of the Interface is different than the Vector Type.";
+		throw error;
+	}
+	cuda_copyToHost(interface->getDataPointer(), data, this->getByteSize());
+}
+
+void CudaVector::activation(float* results, FunctionType functionType)
+{
+	cuda_activation(data, size, vectorType, results, functionType, THREADS_PER_BLOCK);
+}
+
