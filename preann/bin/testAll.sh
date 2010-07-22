63951029abf0482422925576c4c7af0ca37a394e
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2010-07-22 21:29:26 +0000

    git-svn-id: svn://svn.code.sf.net/p/preann/code@63 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

---
+++ preann/bin/testAll.sh
@@ -0,0 +1,3 @@
+#./testMemoryLosses > /home/timon/Desktop/ResultadosEjecuciones/testMemoryLosses.log
+./testLayers > /home/timon/Desktop/ResultadosEjecuciones/testLayers.log
+#./testNeuralNets > /home/timon/Desktop/ResultadosEjecuciones/testNeuralNets.log
