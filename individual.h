bd47b23e40bad1ae402be512e445027f820009fb
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2010-12-09 14:52:08 +0000

    semana sin internet
    git-svn-id: svn://svn.code.sf.net/p/preann/code@129 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

--- individual.h
+++ individual.h
@@ -10,17 +10,19 @@
 
 #include "neuralNet.h"
 
+typedef enum {WEIGH, NEURON, NEURON_INVERTED, LAYER} CrossoverLevel;
+#define CROSSOVER_LEVEL_DIM 4
+typedef enum {UNIFORM, PROPORTIONAL, MULTIPOINT} CrossoverAlgorithm;
+#define CROSSOVER_ALGORITHM_DIM 3
+
 class Individual: public NeuralNet {
 protected:
 	float fitness;
-    void crossoverNeuronsByOutput(Layer* thisLayer, Layer *otherLayer, Interface& outputsBitVector);
-public:
-	Individual(ImplementationType implementationType = C);
-	virtual ~Individual();
 
-	Individual* newCopy();
-	void mutate(unsigned numMutations, float mutationRange);
-	void mutate(float probability, float mutationRange);
+    void crossoverNeuronsByOutput(Layer* thisLayer, Layer *otherLayer, Interface& outputsBitVector);
+    void crossoverNeuronsByInput(Interface** inputsBitVectors, Individual *other);
+	void crossoverLayers(Individual *other, Interface* bitVector);
+
 	void uniformCrossoverWeighs(Individual* other, float probability);
 	void uniformCrossoverNeurons(Individual* other, float probability);
 	void uniformCrossoverNeuronsInverted(Individual *other, float probability);
@@ -29,10 +31,20 @@ public:
 	void multipointCrossoverNeurons(Individual* other, unsigned numPoints);
 	void multipointCrossoverNeuronsInverted(Individual *other, unsigned numPoints);
 	void multipointCrossoverLayers(Individual* other, unsigned numPoints);
-	void crossoverLayers(Individual *other, Interface* bitVector);
+public:
+	Individual(ImplementationType implementationType = C);
+	virtual ~Individual();
+
+	Individual* newCopy();
+	void mutate(unsigned numMutations, float mutationRange);
+	void mutate(float probability, float mutationRange);
+	void uniformCrossover(CrossoverLevel crossoverLevel, Individual* other, float probability);
+	void proportionalCrossover(CrossoverLevel crossoverLevel, Individual* other);
+	void multipointCrossover(CrossoverLevel crossoverLevel, Individual* other, unsigned numPoints);
 
 	float getFitness();
 	void setFitness(float fitness);
+	char checkCompatibility(Individual* other);
 };
 
 #endif /* INDIVIDUAL_H_ */
