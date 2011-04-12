dfc9a1aabe098ea723e826a565ffa32759676f4d
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2011-04-12 17:14:22 +0000

    more options added
    git-svn-id: svn://svn.code.sf.net/p/preann/code@137 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

--- preann/bin/testAll.sh
+++ preann/bin/testAll.sh
@@ -1,3 +1,9 @@
 #./testMemoryLosses > /home/timon/workspace/preann/testResults/testMemoryLosses.log
-./testLayers > /home/timon/workspace/preann/testResults/testLayers.log
+#./testVectors > /home/timon/workspace/preann/testResults/testVectors.log
+#./testLayers > /home/timon/workspace/preann/testResults/testLayers.log
 #./testNeuralNets > /home/timon/workspace/preann/testResults/testNeuralNets.log
+#./preann > /home/timon/workspace/preann/testResults/preann.log
+
+
+#./chronoVectors > /home/timon/workspace/preann/testResults/chronoVectors.log
+./bin/chronoPopulationXor > ./testResults/chronoPopulationXor.log
