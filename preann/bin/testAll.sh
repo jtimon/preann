427c131da63d9fab4415dfa0f1b0e218401242c6
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2011-04-21 15:01:08 +0000

    folder structure and improved makefile
    git-svn-id: svn://svn.code.sf.net/p/preann/code@143 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

--- preann/bin/testAll.sh
+++
@@ -1,9 +0,0 @@
-#./testMemoryLosses > /home/timon/workspace/preann/testResults/testMemoryLosses.log
-#./testVectors > /home/timon/workspace/preann/testResults/testVectors.log
-#./testLayers > /home/timon/workspace/preann/testResults/testLayers.log
-#./testNeuralNets > /home/timon/workspace/preann/testResults/testNeuralNets.log
-#./preann > /home/timon/workspace/preann/testResults/preann.log
-
-
-#./chronoVectors > /home/timon/workspace/preann/testResults/chronoVectors.log
-./bin/chronoPopulationXor > ./testResults/chronoPopulationXor.log
