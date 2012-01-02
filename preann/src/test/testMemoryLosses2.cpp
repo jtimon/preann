5bad2d1269a5c149ca42adfafa321a99e33be65a
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2012-01-02 23:25:25 +0000

    git-svn-id: svn://svn.code.sf.net/p/preann/code@310 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

---
+++ preann/src/test/testMemoryLosses2.cpp
@@ -0,0 +1,172 @@
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#include "loop.h"
+#include "test.h"
+#include "population.h"
+#include "binaryTask.h"
+#include "chronometer.h"
+
+
+unsigned memoryLosses = 0;
+
+void checkAndPrintErrors(string testingClass, Test* test)
+{
+    if(MemoryManagement::getPtrCounter() > 0 || MemoryManagement::getTotalAllocated() > 0){
+        cout << "Memory loss detected testing class " << testingClass << ".\n" << endl;
+        test->printCurrentState();
+        MemoryManagement::printTotalAllocated();
+        MemoryManagement::printTotalPointers();
+        memoryLosses++;
+    }
+}
+
+void testBuffer(Test* test)
+{
+	START_BUFFER
+	END_BUFFER
+
+    checkAndPrintErrors("Buffer", test);
+}
+
+void testBuffer2(Test* test)
+{
+	START_BUFFER
+	END_BUFFER
+
+    checkAndPrintErrors("Buffer", test);
+}
+
+void testConnection(Test* test)
+{
+	START_CONNECTION
+	END_CONNECTION
+
+    checkAndPrintErrors("Connection", test);
+}
+
+void testLayer(Test* test)
+{
+    Layer *layer = new Layer(test->getValue("size"), (BufferType)test->getEnum(ET_BUFFER), FT_IDENTITY, (ImplementationType)test->getEnum(ET_IMPLEMENTATION));
+    layer->addInput(layer->getOutput());
+    layer->addInput(layer->getOutput());
+    layer->addInput(layer->getOutput());
+    delete(layer);
+
+    checkAndPrintErrors("Layer", test);
+}
+
+void testNeuralNet(Test* test)
+{
+	unsigned size = test->getValue("size");
+    BufferType bufferType = (BufferType)test->getEnum(ET_BUFFER);
+    ImplementationType implementationType = (ImplementationType)test->getEnum(ET_IMPLEMENTATION);
+
+    NeuralNet* net = new NeuralNet(implementationType);
+    Interface* input = new Interface(size, bufferType);
+	net->addInputLayer(input);
+	net->addInputLayer(input);
+	net->addInputLayer(input);
+	net->addLayer(size, bufferType, FT_IDENTITY);
+	net->addLayer(size, bufferType, FT_IDENTITY);
+	net->addLayer(size, bufferType, FT_IDENTITY);
+	net->addInputConnection(0, 0);
+	net->addInputConnection(1, 0);
+	net->addInputConnection(2, 0);
+	net->addLayersConnection(0, 1);
+	net->addLayersConnection(0, 2);
+	net->addLayersConnection(1, 2);
+	net->addLayersConnection(2, 0);
+
+    delete(net);
+    delete(input);
+    checkAndPrintErrors("NeuralNet", test);
+}
+
+void testPopulation(Test* test)
+{
+	GET_SIZE
+
+    BufferType bufferType = (BufferType)test->getEnum(ET_BUFFER);
+    ImplementationType implementationType = (ImplementationType)test->getEnum(ET_IMPLEMENTATION);
+
+    Interface* input = new Interface(size, bufferType);
+    Individual* example = new Individual(implementationType);
+	example->addInputLayer(input);
+	example->addInputLayer(input);
+	example->addInputLayer(input);
+	example->addLayer(size, bufferType, FT_IDENTITY);
+	example->addLayer(size, bufferType, FT_IDENTITY);
+	example->addLayer(size, bufferType, FT_IDENTITY);
+	example->addInputConnection(0, 0);
+	example->addInputConnection(1, 0);
+	example->addInputConnection(2, 0);
+	example->addLayersConnection(0, 1);
+	example->addLayersConnection(0, 2);
+	example->addLayersConnection(1, 2);
+	example->addLayersConnection(2, 0);
+    Task* task = new BinaryTask(BO_OR, size, 5);
+    Population* population = new Population(task, example, 5, 20);
+
+    delete(population);
+    delete(example);
+    delete(task);
+    delete(input);
+    checkAndPrintErrors("Population", test);
+}
+
+int main(int argc, char *argv[]) {
+	Chronometer total;
+	total.start();
+	try {
+		Loop* loop;
+		ParametersMap parametersMap;
+		parametersMap.putNumber("initialWeighsRange", 20);
+
+		loop = new RangeLoop("size", 100, 101, 100, NULL);
+		loop = new RangeLoop("outputSize", 1, 4, 2, loop);
+		loop = new EnumLoop("bufferType", ET_BUFFER, loop);
+		loop = new EnumLoop("implementationType", ET_IMPLEMENTATION, loop);
+
+		loop->repeatFunction(testBuffer2, &parametersMap);
+
+
+		Test test;
+		test.putIterator("size", 100, 101, 100);
+		test.putIterator("outputSize", 1, 4, 2);
+		test.putConstant("initialWeighsRange", 20);
+		test.withAll(ET_BUFFER);
+		test.withAll(ET_IMPLEMENTATION);
+		test.printParameters();
+
+		test.simpleTest(testBuffer, "Buffer::memory");
+
+		test.exclude(ET_BUFFER, 1, BT_BYTE);
+		test.printParameters();
+
+		test.simpleTest(testConnection, "Connection::memory");
+		test.simpleTest(testLayer, "Layer::memory");
+		test.simpleTest(testNeuralNet, "NeuralNet::memory");
+		test.putIterator("size", 1, 3, 1);
+		test.putConstant("outputSize", 1);
+		test.simpleTest(testPopulation, "Population::memory");
+
+		printf("Exit success.\n", 1);
+		MemoryManagement::printTotalAllocated();
+		MemoryManagement::printTotalPointers();
+	} catch (string error) {
+		cout << "Error: " << error << endl;
+	} catch (...) {
+		printf("An error was thrown.\n", 1);
+	}
+
+	cout << "Total memory losses: " << memoryLosses << endl;
+	MemoryManagement::printListOfPointers();
+
+	total.stop();
+	printf("Total time spent: %f \n", total.getSeconds());
+	return EXIT_SUCCESS;
+}
