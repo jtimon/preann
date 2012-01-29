226d5ed0249a2f9a1ddcba1eb7d8520197298a5d
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2012-01-29 18:02:33 +0000

    ParametersMap::print() and formatting
    git-svn-id: svn://svn.code.sf.net/p/preann/code@314 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

--- preann/src/common/parametersMap.cpp
+++ preann/src/common/parametersMap.cpp
@@ -13,47 +13,91 @@ ParametersMap::ParametersMap()
 
 ParametersMap::~ParametersMap()
 {
-        tNumbers.clear();
-        tPtrs.clear();
+    tNumbers.clear();
+    tPtrs.clear();
 }
 
 void ParametersMap::putNumber(std::string key, float number)
 {
-        tNumbers[key] = number;
+    tNumbers[key] = number;
 }
 
 float ParametersMap::getNumber(std::string key)
 {
-        if (!tNumbers.count(key)){
-                string error = "ParametersMap::getNumber : There's no number named " + key;
-                throw error;
-        }
-        return tNumbers[key];
+    if (!tNumbers.count(key)) {
+        string error = "ParametersMap::getNumber : There's no number named "
+                + key;
+        throw error;
+    }
+    return tNumbers[key];
 }
 
 void ParametersMap::putPtr(std::string key, void* ptr)
 {
-        tPtrs[key] = ptr;
+    tPtrs[key] = ptr;
 }
 
 void* ParametersMap::getPtr(std::string key)
 {
-        if (!tPtrs.count(key)){
-                string error = "ParametersMap::getPtr : There's no pointer named " + key;
-                throw error;
-        }
-        return tPtrs[key];
+    if (!tPtrs.count(key)) {
+        string error = "ParametersMap::getPtr : There's no pointer named "
+                + key;
+        throw error;
+    }
+    return tPtrs[key];
 }
 
 void ParametersMap::putString(std::string key, std::string str)
 {
-        tStrings[key] = str;
+    tStrings[key] = str;
 }
 std::string ParametersMap::getString(std::string key)
 {
-        if (!tStrings.count(key)){
-                string error = "ParametersMap::getString : There's no string named " + key;
-                throw error;
-        }
-        return tStrings[key];
+    if (!tStrings.count(key)) {
+        string error = "ParametersMap::getString : There's no string named "
+                + key;
+        throw error;
+    }
+    return tStrings[key];
 }
+
+void ParametersMap::print()
+{
+    if (tNumbers.size() > 0) {
+        cout << "Numbers:" << endl;
+        std::map<string, float>::iterator numbersIt;
+        FOR_EACH(numbersIt, tNumbers) {
+            cout << "    " << numbersIt->first << ": " << numbersIt->second
+                    << endl;
+        }
+    }
+
+    if (tPtrs.size() > 0) {
+        cout << "Pointers:" << endl;
+        std::map<string, void*>::iterator ptrsIt;
+        FOR_EACH(ptrsIt, tPtrs) {
+            cout << "    " << ptrsIt->first << ": " << ptrsIt->second << endl;
+        }
+    }
+
+    if (tStrings.size() > 0) {
+        cout << "Strings:" << endl;
+        std::map<string, string>::iterator stringsIt;
+        FOR_EACH(stringsIt, tStrings) {
+            cout << "    " << stringsIt->first << ": " << stringsIt->second
+                    << endl;
+        }
+    }
+    cout << endl;
+}
+
+void ParametersMap::copyTo(ParametersMap* parametersMap)
+{
+
+}
+
+void ParametersMap::copyFrom(ParametersMap* parametersMap)
+{
+
+}
+
--- preann/src/common/parametersMap.h
+++ preann/src/common/parametersMap.h
@@ -10,24 +10,26 @@
 
 #include "util.h"
 
-class ParametersMap {
-        std::map<string, float> tNumbers;
-        std::map<string, void*> tPtrs;
-        std::map<string, string> tStrings;
+class ParametersMap
+{
+    std::map<string, float> tNumbers;
+    std::map<string, void*> tPtrs;
+    std::map<string, string> tStrings;
 public:
-        ParametersMap();
-        virtual ~ParametersMap();
-        void putNumber(std::string key, float number);
-        float getNumber(std::string key);
-        void putPtr(std::string key, void* ptr);
-        void* getPtr(std::string key);
-        void putString(std::string key, std::string str);
-        std::string getString(std::string key);
+    ParametersMap();
+    virtual ~ParametersMap();
+    void putNumber(std::string key, float number);
+    float getNumber(std::string key);
+    void putPtr(std::string key, void* ptr);
+    void* getPtr(std::string key);
+    void putString(std::string key, std::string str);
+    std::string getString(std::string key);
 
-        // All you have
-        void copyTo(ParametersMap* parametersMap);
-        // All you have
-        void copyFrom(ParametersMap* parametersMap);
+    void print();
+    // All you have
+    void copyTo(ParametersMap* parametersMap);
+    // All you have
+    void copyFrom(ParametersMap* parametersMap);
 };
 
 #endif /* PARAMETERSMAP_H_ */
