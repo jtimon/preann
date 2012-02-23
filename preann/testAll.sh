ebfc3e1af57f3fd6c1536f2d7546e258cc34a2c2
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2012-02-23 20:41:01 +0000

    git-svn-id: svn://svn.code.sf.net/p/preann/code@362 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

--- preann/testAll.sh
+++
@@ -1,9 +0,0 @@
-#./bin/testMemoryLosses.exe > /home/timon/workspace/preann/output/log/testMemoryLosses.log
-./bin/testBuffers.exe > /home/timon/workspace/preann/output/log/testBuffers.log
-#./bin/testLayers.exe > /home/timon/workspace/preann/output/log/testLayers.log
-#./bin/testNeuralNets.exe > /home/timon/workspace/preann/output/log/testNeuralNets.log
-#./bin/preann.exe > /home/timon/workspace/preann/output/log/preann.log
-
-
-#./bin/chronoBuffers.exe > /home/timon/workspace/preann/output/log/chronoBuffers.log
-#./bin/chronoPopulationXor.exe > ./output/log/chronoPopulationXor.log
