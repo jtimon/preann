dd7f768828e66797682ae245fc4cdf275b2d19a0
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2012-09-26 16:25:14 +0000

    changes for seth to re-run
    git-svn-id: svn://svn.code.sf.net/p/preann/code@703 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

--- preann/src/test/chronoCalculateAndAdd.cpp
+++ preann/src/test/chronoCalculateAndAdd.cpp
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
     try {
         Util::check(argv[1] == NULL, "You must specify an output directory.");
         ChronoPlotter plotter(argv[1], new RangeLoop(Dummy::SIZE, 512, 8193, 512), "Tiempo (ms)");
-        unsigned repetitions = 200;
+        unsigned repetitions = 500;
 
         plotter.parameters.putNumber(Dummy::WEIGHS_RANGE, 20);
         plotter.parameters.putNumber(Dummy::NUM_INPUTS, 2);
