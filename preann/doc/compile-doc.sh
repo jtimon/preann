f857942988aec84b405a78a90b13bf72bbb0a711
jtimon <jtimon@4c7faa8c-6334-4fdc-ad45-69cacb9c43d3>
2012-09-27 22:44:43 +0000

    uml
    git-svn-id: svn://svn.code.sf.net/p/preann/code@712 4c7faa8c-6334-4fdc-ad45-69cacb9c43d3

---
+++ preann/doc/compile-doc.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+java -jar ~/programas/jar/plantuml.jar "/home/jtimon/workspace/preann/doc/uml.org" 
+#emacs --batch --visit=uml.org --funcall plantuml-render-buffer
+emacs --batch --visit=preann-doc.org -f org-export-as-pdf
+
+
+
